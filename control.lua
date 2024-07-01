a23ae22d89ccd53200d751d18ba2192b87f659ee
Stephen Barnes <stephen6174@gmail.com>
2024-07-01 20:56:45 +0200

    Remove attempt to modify Inspiration costs

--- control.lua
+++
@@ -1,1 +0,0 @@
-require("stages.control.inspiration-costs")
--- stages/data/tweaks/IR3/starting-junkheaps.lua
+++ stages/data/tweaks/IR3/starting-junkheaps.lua
@@ -1,12 +1,15 @@
 -- Make the starting copper/tin junkpiles produce some better products, to jumpstart the game.
 -- IR3 has a setting for starting age, which determines which junkpiles are used. Desolation uses the "stone" setting which starts us with copper/tin junkpiles. Other starting ages already produce non-scrap stuff. So we only modify if we're starting with stone.
 -- But we don't need to care about any of that, just modify those starting junkpiles. Won't affect starting items if a different starting age is used, because the junkpiles we're modifying here won't be placed.
+
+-- Note that these items can trigger inspirations like copper/tin-working 2, even before their prereqs are researched. I tried and failed to change the costs of these in Inspiration, so instead I'll just make sure we don't get many of those inspiration-triggering items here.
+
 local metals = {"copper", "tin"}
 local itemsAmounts = {
 	["scrap"] = {40, 60},
-	["plate"] = {4, 6},
-	["rod"] = {8, 12},
-	["gear-wheel"] = {4, 6},
+	["plate"] = {1, 1},
+	["rod"] = {4, 6},
+	["gear-wheel"] = {1, 1},
 	["rivet"] = {4, 6},
 }
 data.raw["simple-entity"]["copper-tin-junkpile-scrap"].minable.results = {}
@@ -24,4 +27,3 @@ for _, metal in pairs(metals) do
 		end
 	end
 end
--- Note that these items can trigger inspirations like copper/tin-working 2, even before their prereqs are researched. So I need to increase the inspiration cost of those items, which I'll do elsewhere.
