8c6eaecba489587305a1c91539d457d386165b8f
Stephen Barnes <stephen6174@gmail.com>
2024-07-03 00:45:57 +0200

    Add dependency on Harder Basic Logistics, adjust settings

---
+++ code/settings/harder-basic-logistics.lua
@@ -0,0 +1,3 @@
+-- We want to encourage the use of containers, ingot stacking, and loaders. So make belts slower.
+data.raw["double-setting"]["HarderBasicLogistics-belt-speed-multiplier"].default_value = 0.5 -- Reduced 1->0.5.
+data.raw["double-setting"]["HarderBasicLogistics-splitter-speed-multiplier"].default_value = 0.5 -- Reduced 1->0.5.
