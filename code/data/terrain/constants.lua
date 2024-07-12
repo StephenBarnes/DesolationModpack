034a687a0a902444e34f9bd1d0798733caa09892
Stephen Barnes <stephen6174@gmail.com>
2024-07-12 08:00:37 +0200

    Add cold patch on starting island

---
+++ code/data/terrain/constants.lua
@@ -0,0 +1,5 @@
+local startingRegionSize = 1800
+
+return {
+	startingRegionSize = startingRegionSize
+}
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,6 +6,7 @@ require("code.data.terrain.resource-placing")
 require("code.data.terrain.autoplace-controls")
 require("code.data.terrain.terrain-preset")
 require("code.data.terrain.rubber-trees-edit")
+require("code.data.terrain.cold-starting-region")
 
 require("code.data.tweaks.vehicles")
 require("code.data.tweaks.stack-sizes")
