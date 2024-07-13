77d24d3f393e2e17bdc6bab53884ce9337dc0913
Stephen Barnes <stephen6174@gmail.com>
2024-07-13 11:55:45 +0200

    Basic uranium spawning; need to customize, probably copy and edit core's resource-autoplace lib

--- code/data/terrain/resource-placing.lua
+++ code/data/terrain/resource-placing.lua
@@ -69,75 +69,17 @@ end)
 -- All resources that fade in at a certain distance from the starting island.
 
 -- TODO I want these to be partially dependent on elevation, so they spawn near the centers of islands, not overlapping the edges of islands.
-data.raw.resource["uranium-ore"].autoplace.richness_expression = tne(100) -- TODO
-data.raw.resource["uranium-ore"].autoplace.probability_expression = noise.define_noise_function(function(x, y, tile, map)
-	local scale = 1 / (C.terrainScaleSlider * map.segmentation_multiplier)
+data.raw.resource["uranium-ore"].autoplace = resourceAutoplace.resource_autoplace_settings {
+	name = "uranium-ore",
+	order = "c",
+	base_density = 0.9,
+	base_spots_per_km2 = 1.25,
+	has_starting_area_placement = false,
+	random_spot_size_minimum = 2,
+	random_spot_size_maximum = 4,
+	regular_rq_factor_multiplier = 1
+}
 
-	if false then
-		local frequency_multiplier = noise.var("control-setting:uranium-ore:frequency:multiplier")
-		local size_multiplier = noise.var("control-setting:uranium-ore:size:multiplier")
-		local density_multiplier = frequency_multiplier * size_multiplier
-		local base_density = 10
-		local double_density_distance = 1300 -- distance at which patches have twice as much stuff in them
-		-- Maximum distance at which blob amplitude should keep increasing along with spot height
-		local regular_blob_amplitude_maximum_distance = double_density_distance
-		local spot_enlargement_maximum_distance = regular_blob_amplitude_maximum_distance
-		local function regular_density_at(dist)
-			-- Don't increase density beyond spot_enlargement_maximum_distance
-			-- because large spots get unwieldy.  We'll increase richness after that, instead.
-			effective_distance = noise.clamp(dist, 0, spot_enlargement_maximum_distance)
-			local distance_density_multiplier = 1 + effective_distance / double_density_distance
-			return base_density * density_multiplier * distance_density_multiplier
-		end
-		local base_spots_per_km2 = 2.5
-		local spots_per_km2_near_start = base_spots_per_km2 * frequency_multiplier
-		-- Regular spot quantity without randomization added
-		local function regular_spot_quantity_base_at(dist)
-			return regular_density_at(dist) * 1000000 / spots_per_km2_near_start
-		end
-		local random_spot_size_minimum = 0.25
-		local random_spot_size_maximum = 2.0
-		-- Regular spot quantity averaging over randomization
-		local function regular_spot_quantity_typical_at(dist)
-			local average_random_size_multiplier = (random_spot_size_minimum + random_spot_size_maximum) / 2
-			return average_random_size_multiplier * regular_spot_quantity_base_at(dist)
-		end
-		local regular_rq_factor = 1/10
-		local function regular_spot_height_typical_at(dist)
-			return regular_spot_quantity_typical_at(dist)^(1/3) / ((math.pi/3) * regular_rq_factor^2)
-		end
-		local function regular_blob_amplitude_at(dist)
-			return noise.min(
-				regular_spot_height_typical_at(regular_blob_amplitude_maximum_distance),
-				regular_spot_height_typical_at(dist)
-			)
-		end
-		--local basement_value = regular_blob_amplitude_at(regular_blob_amplitude_maximum_distance)
-		basement_value = -1000
-
-		local spotProb = tne{
-			type = "function-application",
-			function_name = "factorio-basis-noise",
-			arguments = {
-				x = noise.var("x") / scale + C.artifactShift,
-				y = noise.var("y") / scale,
-				seed0 = noise.var("map_seed"),
-				seed1 = tne(Util.getNextSeed1()),
-				region_size = 1024,
-				density_expression = tne(600),
-				spot_quantity_expression = noise.random_between(0.25, 1) * 600 * 1000000 / (2.5 * noise.var("control-setting:uranium-ore:frequency:multiplier")),
-				spot_radius_expression = tne(32),
-				hard_region_target_quantity = tne(false),
-				spot_favorability_expression = tne(1),
-				basement_value = basement_value,
-				maximum_spot_basement_radius = tne(128),
-			}
-		}
-		return spotProb
-	end
-
-	
-end)
 
 ------------------------------------------------------------------------
 
