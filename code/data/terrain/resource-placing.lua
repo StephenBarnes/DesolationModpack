d0cdf5c9bf897c89173f9c7bdc26753907d4c541
Stephen Barnes <stephen6174@gmail.com>
2024-07-13 09:46:03 +0200

    Refactor iron starting patch

--- code/data/terrain/resource-placing.lua
+++ code/data/terrain/resource-placing.lua
@@ -4,8 +4,17 @@ local tne = noise.to_noise_expression
 local Util = require("code.data.terrain.util")
 local C = require("code.data.terrain.constants")
 
--- Define a starting iron factor, between 0 and 1. This is used for both probability and richness.
---local startingIronFactor = noise.define_noise_function(function(x, y, tile, map)
+-- Define a starting patch iron factor, between 0 and 1. This is used for both probability and richness.
+local startingPatchIronFactor = noise.define_noise_function(function(x, y, tile, map)
+	local scale = 1 / (C.terrainScaleSlider * map.segmentation_multiplier)
+	local startingIronPos = Util.getStartIslandIronCenter(scale)
+	local distFromStartIron = Util.dist(startingIronPos[1], startingIronPos[2], x, y) / scale
+	local factor = Util.rampDouble(distFromStartIron,
+		C.startIronPatchMinRad, C.startIronPatchMidRad, C.startIronPatchMaxRad,
+		C.startIronPatchCenterWeight, 0, -C.startIronPatchCenterWeight)
+	local factorNoise = Util.multiBasisNoise(2, 2, 2, 1 / (scale * C.startIronPatchProbNoiseInputScale), C.startIronPatchProbNoiseAmplitude)
+	return factor + factorNoise
+end)
 
 local originalIronProb = data.raw.resource["iron-ore"].autoplace.probability_expression
 data.raw.resource["iron-ore"].autoplace.probability_expression = noise.define_noise_function(function(x, y, tile, map)
@@ -18,34 +27,17 @@ data.raw.resource["iron-ore"].autoplace.probability_expression = noise.define_no
 	modifiedIronProb = noise.min(maxProbForStartIsland, modifiedIronProb)
 
 	-- Layer that's high at the starting iron patch, 0 everywhere else.
-	local startingIronPos = Util.getStartIslandIronCenter(scale)
-	local distFromStartIron = Util.dist(startingIronPos[1], startingIronPos[2], x, y) / scale
-
-	local minProbForStartIronPatch = Util.rampDouble(distFromStartIron,
-		C.startIronPatchMinRad, C.startIronPatchMidRad, C.startIronPatchMaxRad,
-		C.startIronPatchCenterWeight, 0, -C.startIronPatchCenterWeight)
-	local probNoise = Util.multiBasisNoise(2, 2, 2, 1 / (scale * C.startIronPatchProbNoiseInputScale), C.startIronPatchProbNoiseAmplitude)
-	local minProbForStartWithNoise = noise.clamp(probNoise + minProbForStartIronPatch, 0, 1)
+	local minProbForStartWithNoise = noise.clamp(startingPatchIronFactor, 0, 1)
 	local floorProb = 0.8 -- Set probability to 0 if it's below this value, so we don't get spatterings of ore like the spray can tool in MS Paint.
 	minProbForStartWithNoise = noise.if_else_chain(noise.less_than(minProbForStartWithNoise, floorProb), 0, minProbForStartWithNoise)
 	modifiedIronProb = noise.max(minProbForStartWithNoise, modifiedIronProb)
-	--modifiedIronProb = noise.max(minProbForStartIronPatch, modifiedIronProb)
 
 	return modifiedIronProb
 end)
 
 local originalIronRichness = data.raw.resource["iron-ore"].autoplace.richness_expression
 data.raw.resource["iron-ore"].autoplace.richness_expression = noise.define_noise_function(function(x, y, tile, map)
-	local scale = 1 / (C.terrainScaleSlider * map.segmentation_multiplier)
-	local startingIronPos = Util.getStartIslandIronCenter(scale)
-	local distFromStartIron = Util.dist(startingIronPos[1], startingIronPos[2], x, y) / scale
-
-	local richnessNoise = Util.multiBasisNoise(2, 2, 2, 1 / (scale * C.startIronPatchRichnessNoiseInputScale), tne(C.startIronPatchRichnessNoiseAmplitude))
-	local richnessRampBase = Util.rampDouble(distFromStartIron,
-		0, C.startIronPatchMidRad, C.startIronPatchMaxRad,
-		5000, 1000, -5000)
-	local richness = richnessNoise + richnessRampBase
-
+	local richness = 400 * startingPatchIronFactor * noise.var("control-setting:iron-ore:richness:multiplier")
 	return noise.if_else_chain(noise.less_than(1, richness), richness, originalIronRichness)
 end)
 
