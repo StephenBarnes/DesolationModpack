24f505418c92406aac82a68a0f7c0f4f628e644a
Stephen Barnes <stephen6174@gmail.com>
2024-07-13 13:48:08 +0200

    Add better ice graphics

--- code/data/tweaks/ore-compounds.lua
+++ code/data/tweaks/ore-compounds.lua
@@ -5,14 +5,22 @@
 -- We could make each mining round output like 2x ore, 1x stone, 1x ice. However, that makes the miners get stuck outputting their ores. So rather use probabilities.
 
 -- Create ice item
+-- Pictures are vanilla Factorio's icons for uranium-ore, but colorized and with a color-to-alpha filter to make them partially transparent.
 data:extend({{
 	type = "item",
 	name = "ice",
-	icon = "__Desolation__/graphics/ice.png",
+	icon = "__Desolation__/graphics/ice/ice.png",
 	icon_size = 64,
+	icon_mipmaps = 4,
 	subgroup = "ore",
 	order = "aa-bb",
 	stack_size = data.raw.item["stone"].stack_size,
+	pictures = { -- Multiple pictures, to make it not look so uniform on the belts.
+		{ filename = "__Desolation__/graphics/ice/ice.png", size = 64, scale = 0.25, mipmap_count = 4 },
+		{ filename = "__Desolation__/graphics/ice/ice-1.png", size = 64, scale = 0.25, mipmap_count = 4 },
+		{ filename = "__Desolation__/graphics/ice/ice-2.png", size = 64, scale = 0.25, mipmap_count = 4 },
+		{ filename = "__Desolation__/graphics/ice/ice-3.png", size = 64, scale = 0.25, mipmap_count = 4 },
+	}
 }})
 
 -- Adjust ores so that they also produce ice and stone.
@@ -27,7 +35,7 @@ for _, resourceName in pairs({"iron-ore", "copper-ore", "tin-ore", "coal"}) do
 end
 
 data.raw.resource["stone"].minable.results = {
-	{type = "item", name = "stone", probability = 1/2, amount = 1},
-	{type = "item", name = "ice", probability = 1/2, amount = 1},
+	{type = "item", name = "stone", probability = 3/4, amount = 1},
+	{type = "item", name = "ice", probability = 1/4, amount = 1},
 }
 -- data.raw.resource["stone"].minable.mining_time = data.raw.resource["stone"].minable.mining_time * 3
