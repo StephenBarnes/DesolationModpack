2cc64676bded20bb1aaae6ff9210baefc89299ea
Stephen Barnes <stephen6174@gmail.com>
2024-07-19 13:52:36 +0200

    Add notes, cargo ships settings

---
+++ code/settings/cargo-ships.lua
@@ -0,0 +1,4 @@
+local Settings = require("code.util.settings")
+
+-- Disable the offshore oil and oil rig, bc our progression is rather built on island-hopping.
+Settings.setDefault("deep_oil", "bool", false)
