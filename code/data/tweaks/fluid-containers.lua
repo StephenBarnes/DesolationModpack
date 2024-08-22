a21f5d6daf6ce761ce6ea96342b50ef82ef4ff9a
Stephen Barnes <stephen6174@gmail.com>
2024-08-22 18:54:52 +0200

    Add notes on fluid containers

--- code/data/tweaks/fluid-containers.lua
+++ code/data/tweaks/fluid-containers.lua
@@ -1,15 +1,18 @@
+-- Modify stack sizes and amounts contained in barrels and canisters.
 
--- Modify stack size and contents of barrels and canisters?
--- Copper canisters hold 100 steam and have stack size 50, so 5000/stack, or 400k/cargowagon.
--- Iron canisters hold 30 fluid and have stack size 50, so 1500/stack, or 120k/cargowagon. Researched soon after iron+electricity. Made from iron. Holds only petroleum and steam.
--- Barrels contain 60 fluid and have stack size 10, so 600/stack, or 48k/cargowagon. Researched right after steel. Made from steel. Holds petroleum and most liquids.
--- High-pressure canisters hold 80 fluid and have stack size 10, so 800/stack, or 64k/cargowagon. Research comes later than barrels. Made from steel and nanoglass. Holds gases. No need to change this since nothing else transports all those gases, except fluid wagons.
--- Fluid wagon holds 25k fluid. Cargo wagon holds 80 stacks.
--- So with the default settings:
--- * Transporting steam is best in copper canisters, but you could also use fluid wagons for simplicity, or barrels (since sending empty steam canisters back is a logistical problem), or even iron canisters (if you've got them moving around anyway for petroleum).
--- * Transporting petroleum is best in iron canisters, but you could also use fluid wagons for simplicity, or barrels (since sending empty iron canisters back is a logistical problem).
--- * Transporting most liquids is best in barrels (for double throughput) or fluid wagons (for simplicity).
--- * Transporting gases is best in high-pressure canisters (for 2.5x throughput) or fluid wagons (for simplicity).
--- In summary, I don't think anything here needs changing.
+--[[ Considering various fuel types etc.
+Steam:
+	Fuel value is 3 MJ per 100 steam.
+	Steam cell (made from 1.5 copper ingots) holds 100 steam, which is 3 MJ.
+		Stack size 10, so 1000 steam per stack, so 30 MJ per stack.
+	Steam canister (made from 2.5 iron ingots) holds 200 steam, which is 6 MJ.
+		Stack size 10, so 2000 steam per stack, so 60 MJ per stack.
+Petroleum:
+	Fuel value is 4MJ per 10 petroleum.
+	Petroleum canister holds 30 petroleum = 12 MJ.
+		Stack size 10, so 300 petroleum per stack, so 120 MJ per stack.
+	Barrel holds 60 petroleum = 24 MJ.
+		Stack size 10, so 600 petroleum per stack, so 240 MJ per stack.
 
--- TODO review all this.
+TODO I think this requires playtesting. Otherwise it's hard to know how reasonable it is to have eg 60 chrome plating solution per barrel, 600 per stack, etc.
+]]
