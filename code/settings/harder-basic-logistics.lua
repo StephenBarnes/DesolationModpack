f72c2ac1bd70a8878d32759dc580348ca509b161
Stephen Barnes <stephen6174@gmail.com>
2024-08-29 21:08:46 +0200

    Made dependency on HarderBasicLogistics optional

--- code/settings/harder-basic-logistics.lua
+++ code/settings/harder-basic-logistics.lua
@@ -1,4 +1,6 @@
 local Settings = require("code.util.settings")
-Settings.setDefaultOrForce("HarderBasicLogistics-inserter-placement-blocking", "string", "allow-all")
-Settings.setDefaultOrForce("HarderBasicLogistics-remove-long-inserters", "bool", true)
-Settings.setDefaultOrForce("HarderBasicLogistics-shorten-underground-belts", "string", "all-1")
+if mods["HarderBasicLogistics"] ~= nil then
+	Settings.setDefaultOrForce("HarderBasicLogistics-inserter-placement-blocking", "string", "allow-all")
+	Settings.setDefaultOrForce("HarderBasicLogistics-remove-long-inserters", "bool", true)
+	Settings.setDefaultOrForce("HarderBasicLogistics-shorten-underground-belts", "string", "all-1")
+end
